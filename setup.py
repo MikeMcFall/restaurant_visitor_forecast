5ee75e5d893dfa6487f194c58a69de6bcad661d7
Michael McFall <michaeljmcfall1@gmail.com>
2020-04-10 16:52:24 -0400

    Initial commit
    Initial commit with file structure based on Cookiecutter Data Science

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My analysis and predictive model for the Kaggle competition. '
+                'Completed as part of the Springboard curriculum.',
+    author='Michael McFall',
+    license='BSD-3',
+)
